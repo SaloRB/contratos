d5e316462424b2651340d251e4656ce2e9e301f6
Salomon Romano <salo_romano@yahoo.com>
2018-08-02 00:35:07 -0500

    Correccion de inicio de sesion y diseño de forma

--- app/controllers/sesiones_controller.rb
+++ app/controllers/sesiones_controller.rb
@@ -6,15 +6,15 @@ class SesionesController < ApplicationController
 	  usuario = Usuario.find_by(email: params[:email])
 	  if usuario && usuario.authenticate(params[:password])
 	    session[:usuario_id] = usuario.id
-	    redirect_to root_url, notice: 'Welcome ' << usuario.email << '!'
+	    redirect_to root_url, notice: '¡Bienvenido ' << usuario.email << '!'
 	  else
-	    flash.now.alert = 'Email or password is invalid'
+	    flash.now.notice = 'Usuario o contraseña inválido'
 	    render :new
 	  end
 	end
 
 	def destroy
 		session[:usuario_id] = nil
-		redirect_to root_url, notice: 'Logged out!'
+		redirect_to root_url, notice: '¡Sesión finalizada!'
 	end
 end
