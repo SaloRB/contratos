45b1ae6312ee0055b5ac691fbafeac715c05027c
Salomon Romano <salo_romano@yahoo.com>
2018-09-03 21:39:07 -0500

    Ajustes

--- app/models/tipo_entidad.rb
+++ app/models/tipo_entidad.rb
@@ -1,4 +1,4 @@
 class TipoEntidad < ApplicationRecord
 	has_many :entidades
-	validates :tipo, presence: true, length: { maximum: 20}, uniqueness: true
+	validates :tipo, presence: true, length: { maximum: 20 }, uniqueness: true
 end
