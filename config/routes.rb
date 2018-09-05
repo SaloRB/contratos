463f2accbf17d6ecbe00301da30dbf7e8f103fef
Salomon Romano <salo_romano@yahoo.com>
2018-09-05 14:39:49 -0500

    Signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   root 		'welcome#index'
-  get 		'signup', to: 'usuarios#new',     as: 'signup'
-  get 		'/login', to: 'sesiones#new'
-  post 		'/login', to: 'sesiones#create'
-  delete 	'/logout', to: 'sesiones#destroy'
+  get 		'/signup',  to: 'usuarios#new'
+  post    '/signup',  to: 'usuarios#create'
+  get 		'/login',   to: 'sesiones#new'
+  post 		'/login',   to: 'sesiones#create'
+  delete 	'/logout',  to: 'sesiones#destroy'
   resources :usuarios
   resources :entidades
   resources :tipo_entidades
